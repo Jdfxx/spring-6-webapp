6397681af46cf8e02908a81fbe07c26384a32ec8
Filip <fhagno@yahoo.com>
2025-02-04 11:54:08 +0100

    Add publisher entity

--- src/main/java/pl/filiphagno/spring_6_webapp/boostrap/BoostrapData.java
+++ src/main/java/pl/filiphagno/spring_6_webapp/boostrap/BoostrapData.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.filiphagno.spring_6_webapp.domain.Author;
 import pl.filiphagno.spring_6_webapp.domain.Book;
+import pl.filiphagno.spring_6_webapp.domain.Publisher;
 import pl.filiphagno.spring_6_webapp.repositories.AuthorRepository;
 import pl.filiphagno.spring_6_webapp.repositories.BookRepository;
+import pl.filiphagno.spring_6_webapp.repositories.PublisherRepository;
 
 @Component
 public class BoostrapData implements CommandLineRunner {
@@ -13,13 +15,25 @@ public class BoostrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
 
-    public BoostrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    private final  PublisherRepository publisherRepository;
+
+    public BoostrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                        PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher pub1 = new Publisher();
+        pub1.setPublisherName("Some publisher");
+        pub1.setAddress("Some street");
+        pub1.setCity("Some city");
+        pub1.setZip("Some zip");
+        pub1.setState("Some state");
+
         Author eric = new Author();
         eric.setFirstName("Eric");
         eric.setLastName("Evans");
@@ -30,6 +44,7 @@ public class BoostrapData implements CommandLineRunner {
 
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
+        Publisher pub1Saved = publisherRepository.save(pub1);
 
         Author rod = new Author();
         rod.setFirstName("Rod");
@@ -44,12 +59,20 @@ public class BoostrapData implements CommandLineRunner {
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+        pub1Saved.getBooks().add(dddSaved);
+        pub1Saved.getBooks().add(noEJBSaved);
+        dddSaved.setPublisher(pub1Saved);
+        noEJBSaved.setPublisher(pub1Saved);
 
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
+        publisherRepository.save(pub1Saved);
+        bookRepository.save(noEJBSaved);
+        bookRepository.save(dddSaved);
 
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
---
+++ src/main/java/pl/filiphagno/spring_6_webapp/domain/Publisher.java
@@ -0,0 +1,92 @@
+package pl.filiphagno.spring_6_webapp.domain;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+    private String publisherName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books = new HashSet<>();
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return Id == publisher.Id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (Id ^ (Id >>> 32));
+    }
+}
